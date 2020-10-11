5e2a7966480f325e56294186fba5a6c9c25f06b2
frontman <a.frontman.blog@gmail.com>
2020-10-11 13:52:08 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstModule } from './modules/first/first.module';
-import { SecondModule } from './modules/second/second.module';
 import { GlobalErrorHandler } from './error-handler/global-error-handler';
 
 @NgModule({
@@ -13,8 +11,6 @@ import { GlobalErrorHandler } from './error-handler/global-error-handler';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FirstModule,
-    SecondModule
   ],
   providers: [
     { provide: ErrorHandler, useClass: GlobalErrorHandler }
